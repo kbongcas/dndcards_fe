32fd0f4a17c9762849af44860d0b794b67d290ee
kbongcasmis <kbongcasmis@gmail.com>
2018-03-30 21:07:55 -0500

    Setup angular project and set up Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+      BrowserAnimationsModule,
+      MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
