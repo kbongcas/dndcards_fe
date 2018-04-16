9544a7b8e1628c859288b2fb9cf0e1e0465410da
kbongcasmis <kbongcasmis@gmail.com>
2018-04-16 09:09:48 -0500

    Added spell for to dialog box

--- src/app/spell-form/spell-form.component.ts
+++ src/app/spell-form/spell-form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormBuilder } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Spell } from '../spell-service/spell';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-spell-form',
@@ -13,6 +15,7 @@ export class SpellFormComponent implements OnInit {
   description: string;
   fb: FormBuilder;
   dialogRef: MatDialogRef<SpellFormComponent>;
+  spell: Spell;
 
   constructor(
     fb: FormBuilder,
@@ -26,6 +29,19 @@ export class SpellFormComponent implements OnInit {
   ngOnInit() {
     this.form = this.fb.group({
       description: [this.description, []],
+      spellId: ['3231',[]],
+      spellName: ['',[]],
+      isRitual: ['',[]],
+      ritualName: ['',[]],
+      level: ['',[]],
+      castingTime: ['',[]],
+      range: ['',[]],
+      duration: ['',[]],
+      components: ['',[]],
+      materials: ['',[]],
+      spellDesc: ['',[]],
+      higherLevelDescOne: ['',[]],
+      higherLevelDescTwo:  ['',[]]
     });
   }
 
