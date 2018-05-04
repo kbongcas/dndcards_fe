640cf58e7634aa3a3654fdc1c82691b5912a5a1c
kbongcasmis <kbongcasmis@gmail.com>
2018-05-04 00:37:50 -0500

    added pretty edit

--- src/app/spell-view/spell-view.component.ts
+++ src/app/spell-view/spell-view.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Inject } from '@angular/core';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { Spell } from '../spell-service/spell';
 import {FormControl, Validators, FormBuilder} from '@angular/forms';
 import { SpellFormComponent } from '../spell-form/spell-form.component';
@@ -13,17 +13,32 @@ export class SpellViewComponent implements OnInit {
 
   dialogRef: MatDialogRef<SpellViewComponent>;
   spell: Spell;
-
+  dialog: MatDialog;
   constructor(
+    dialog: MatDialog,
     dialogRef: MatDialogRef<SpellViewComponent>,
     @Inject(MAT_DIALOG_DATA) spell: Spell) {
     this.dialogRef = dialogRef;
     this.spell = spell;
+    this.dialog = dialog;
   }
   ngOnInit() {
   }
 
   close() {
   this.dialogRef.close();
+  }
+
+  openEditSpellDialog(spell: Spell): void{
+      this.spell = spell;
+      const dialogConfig = new MatDialogConfig();
+
+      dialogConfig.disableClose = true;
+      dialogConfig.autoFocus = true;
+
+      dialogConfig.data =this.spell;
+      const dialogRef = this.dialog.open(SpellFormComponent, dialogConfig);
+      dialogRef.afterClosed().subscribe(
+      spell => this.dialogRef.close(this.spell))
   }
 }
